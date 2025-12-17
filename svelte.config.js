b6c3262207ff9ebb6d17ecde5ec60e93e1994bb6
ajuvercr <arthur.vercruysse@outlook.com>
2025-12-17 09:53:53 +0100

    update svelte config add base path

--- svelte.config.js
+++ svelte.config.js
@@ -23,7 +23,10 @@ const config = {
 			fallback: undefined,
 			precompress: false,
 			strict: true
-		})
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
 	}
 };
 
